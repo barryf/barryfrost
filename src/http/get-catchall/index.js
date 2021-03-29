512cc6697d19768d78b8d6b3e310c872ed5ef741
Barry Frost <public@barryfrost.com>
2021-03-29 13:11:15 +0100

    Redirect /feed to /rss

--- src/http/get-catchall/index.js
+++ src/http/get-catchall/index.js
@@ -243,8 +243,8 @@ async function handleUrl (url, params) {
   } else if (url.slice(0, 5) === 'tags/') {
     const category = url.slice(5)
     return redirect301(`categories/${category}`)
-  // redirect index.xml => rss
-  } else if (url === 'index.xml') {
+  // redirect index.xml and feed => rss
+  } else if (url === 'index.xml' || url === 'feed') {
     return redirect301('rss')
   // categories as javascript array
   } else if (url === 'categories.js') {
