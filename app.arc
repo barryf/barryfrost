de4e9d0a219cb491d1ab9aa9ecb02e27d1b17401
Barry Frost <public@barryfrost.com>
2021-03-29 14:55:22 +0100

    Proxy image for legacy /barryfrost.jpg

--- app.arc
+++ app.arc
@@ -5,6 +5,7 @@ barryfrost
 region eu-west-2
 
 @http
+get /barryfrost.jpg
 get /map/*
 get /rss
 get /*
---
+++ src/http/get-barryfrost_jpg/index.js
@@ -0,0 +1,25 @@
+// image proxy for legacy /barryfrost.jpg image
+
+const fetch = require('node-fetch')
+const arc = require('@architect/functions')
+
+async function fetchImage () {
+  const url = new URL(arc.static('/barryfrost.jpg'), process.env.ROOT_URL)
+  const response = await fetch(url)
+  const buffer = await response.buffer()
+  const image = Buffer.from(buffer).toString('base64')
+  return image
+}
+
+exports.handler = async function http (req) {
+  const image = await fetchImage()
+  return {
+    statusCode: 200,
+    headers: {
+      'Content-Type': 'image/jpeg',
+      'Cache-Control': 's-maxage=86400' // 1 day
+    },
+    body: image.toString('base64'),
+    isBase64Encoded: true
+  }
+}
